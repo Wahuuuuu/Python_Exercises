0b447d185224325850b1488b80f213b97a800d31
Wahuuuuu <2080559692@qq.com>
2026-01-13 18:55:06 +0000

    I'll sove it in two days

---
+++ categories/leetcode/3453.separate-squares-i.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=3453 lang=python3
+#
+# [3453] Separate Squares I
+#
+
+# @lc code=start
+class Square:
+    def __init__(self, x, y, l):
+        self.x = x
+        self.y = y
+        self.side = l
+        
+
+class Solution:
+    def init_lower_higher(self, squares: list[list[int]]) -> tuple[Square]:
+        
+
+
+    def separateSquares(self, squares: list[list[int]]) -> float:
+        lower, higher = self.init_lower_higher(squares)  # square which's base is lower and higher
+        
+
+# @lc code=end
+
